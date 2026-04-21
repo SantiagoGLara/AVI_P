28ffcc619047161f704d3456d5ff5842678995cd
DaBriNan <brisenopompadaniela@gmail.com>
2026-04-21 00:28:01 -0600

    style: :sparkles: front implementado
    has click derecho en la carpeta de front/index y lo abres con lifeServer para que puedas ver el front, esta sencillo =, tenemos 3 pestañaas para el analisis, reporte y hacer preguntas

--- app/main.py
+++ app/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes.analysis import router as analysis_router
 from app.routes.upload import router as upload_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(analysis_router)
 app.include_router(upload_router)
